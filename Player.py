d68f2dcd8c0c38cd61908ed4030b70d1b50575d1
Piotr Grygoruk <grygorukpp@gmail.com>
2023-01-24 11:29:48 +0100

    player refactoring

--- Player.py
+++ Player.py
@@ -1,12 +1,14 @@
 class Player:
-    def __init__(self, rfid, name, wins, defeats):
+    def __init__(self, rfid, name, wins, defeats, draws):
         self.rfid = rfid
         self.name = name
         self.wins = wins
         self.defeats = defeats
+        self.draws = draws
 
     def __str__(self) -> str:
         return f'RFID: {self.rfid}\n' \
                f'Name: {self.name}\n' \
                f'Wins: {self.wins}\n' \
-               f'Defeats: {self.defeats}\n'
+               f'Defeats: {self.defeats}\n' \
+               f'Draws: {self.draws}\n'
